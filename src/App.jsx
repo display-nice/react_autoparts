a2755c4775f2e0c29f093ace064599aa5a25839b
wakadaka59 <osrolex@yandex.ru>
2024-08-20 17:46:27 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./visuals/css/style.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="header">
+				<div className="header__topline">
+					<div className="header__topline-left"></div>
+					<div className="header__topline-right"></div>
+				</div>
+				<div className="header__botline">
+					<div className="fake-mobile-search-wrp"></div>
+				</div>
+			</header>
+			<main></main>
+			<footer></footer>
+		</div>
+	);
+}
+
+export default App;
