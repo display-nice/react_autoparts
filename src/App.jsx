0143c7541b167d51716a7870b755f395f2463cb6
wakadaka59 <osrolex@yandex.ru>
2024-08-28 19:09:11 +0500

    +promoslider

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import AuthIcon from "./visuals/svg/Sign_in.svg";
 import CartIcon from "./visuals/svg/Cart.svg";
 import CatalogIcon from "./visuals/svg/Main_burger.svg";
 import DropdownIcon from "./visuals/svg/Arrow_Dropdown.svg";
+import ArrowRightBlue from "./visuals/svg/Arrow_Right_Blue.svg";
+import ArrowRightWhite from "./visuals/svg/Arrow_Right_White.svg";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="header">
 				<div className="header__topline">
-					<div className="header__container header__container--top">
+					<div className="frame header__container-top">
 						<nav className="header__topline-info">
 							<div className="header__nav-xs">
 								<img src={NavIcon} alt="Навигация" />
@@ -30,7 +32,7 @@ function App() {
 				</div>
 
 				<div className="header__botline">
-					<div className="header__container header__container--bot">
+					<div className="frame header__container-bot">
 						<div className="header__botline-search-xs">
 							<img src={SearchIcon} alt="" />
 							<input
@@ -50,11 +52,50 @@ function App() {
 						</button>
 					</div>
 				</div>
-
-				{/* <div className="header__container"></div> */}
 			</header>
-			<main className="main"></main>
-			<footer></footer>
+			<main className="main">
+				<section className="promoslider">
+					<picture className="promoslider__bg">
+						<source srcSet="/img/promoslider/1600+.jpg" media="(min-width: 1600px)" />
+						<source srcSet="/img/promoslider/1280-1600.jpg" media="(min-width: 1280px)" />
+						<source srcSet="/img/promoslider/640-1280.jpg" media="(min-width: 640px)" />
+						<img src="/img/promoslider/320-640.jpg" alt="." />
+					</picture>
+
+					<div className="promoslider__container">
+						<div className="promoslider__description">
+							<img className="promoslider__logo" src="/img/promoslider/logo.jpg" alt="Лого" />
+
+							<div className="promoslider__maintext">
+								Фильтр масла <br />в подарок
+							</div>
+
+							<button className="promoslider__moredetails button_reset">
+								<span>Подробнее</span>
+								<img src={ArrowRightBlue} alt="" />
+							</button>
+						</div>
+
+						<picture className="promoslider__product">
+							<source srcSet="/img/promoslider/oil_640+.png" media="(min-width: 640px)" />
+							<img src="/img/promoslider/oil_320-640.png" alt="Продукт" />
+						</picture>
+
+						<div className="promoslider__controlbtns">
+							<button className="button_reset promoslider__btn promoslider__btn--left">
+								<img src={ArrowRightWhite} alt="" />
+							</button>
+							<button className="button_reset promoslider__btn promoslider__btn--right promoslider__btn--active">
+								<img src={ArrowRightWhite} alt="" />
+							</button>
+						</div>
+						
+					</div>
+				</section>
+				<section className="services"></section>
+				<section className="showcase"></section>
+			</main>
+			<footer className="footer"></footer>
 		</div>
 	);
 }
